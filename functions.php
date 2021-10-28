adc27229a822d996b443b6e42edbcd88d83a96f5
MarcJandt <marc@jandt.net>
2021-10-28 09:55:15 +0200

    Reverted to default.

--- functions.php
+++ functions.php
@@ -10,6 +10,4 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_register_style( 'uku-style',  get_stylesheet_uri(), [ 'uku-serif-style' ] );
-    wp_enqueue_style( 'uku-style' );
 }
